c71103f10d870c370884d63f81d36afa1d9e2d46
Marcin <stev1989@gmail.com>
2023-06-18 23:17:30 +0200

    commit DriverFactory.java adjust for browser paramater in Jenkins

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -49,12 +49,18 @@ public class DriverFactory {
     }
     public static String getBrowserType() {
         String browserType = null;
+        String browserTypeRemoteValue = System.getProperty("browserType");
 
         try {
-            Properties properties = new Properties();
-            FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/properties/config.properties");
-            properties.load(file);
-            browserType = properties.getProperty("browser").toLowerCase().trim();
+            if (browserTypeRemoteValue==null || browserTypeRemoteValue.isEmpty()){
+                Properties properties = new Properties();
+                FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/properties/config.properties");
+                properties.load(file);
+                browserType = properties.getProperty("browser").toLowerCase().trim();
+            } else {
+                browserType = browserTypeRemoteValue;
+            }
+
         } catch (IOException ex){
             System.out.println(ex.getMessage());
         }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -3,7 +3,7 @@ Feature: WebDriver University - Login Page
 
   Background:
     Given I access webdriver university login page
-  @tag
+  @target
   Scenario: Validate Successful Login
     When I enter a username webdriver
     And I enter a password webdriver123
@@ -16,7 +16,7 @@ Feature: WebDriver University - Login Page
     And I click on login button
     Then I should be presented with a unsuccessful validation info
 
-
+  @ignore
   Scenario Outline: Validate Successful & Unsuccessful Login
     When I enter an outlined username <username>
     And I enter an outlined password <password>
